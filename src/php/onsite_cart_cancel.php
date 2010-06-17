8de64fc232cb750ea0f8983209d860242dee5f1f
James Whitfield <ghost@GERONIMO.local>
2010-06-17 19:17:49 -0400

    added real world senario for onsite cart

---
+++ src/php/onsite_cart_cancel.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <title>Payment Canceled</title>
+        <script type="text/javascript">
+            window.parent.paymentCanceled();
+
+        </script>
+    </head>
+    <body>
+        <h1> Payment Canceled </h1>
+
+    </body>
+</html>
---
+++ src/php/onsite_cart_return.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <title>Payment Complete</title>
+        <script type="text/javascript">
+            window.parent.paymentComplete();
+
+        </script>
+    </head>
+    <body>
+     <h1> Payment Complete </h1>
+    </body>
+</html>
