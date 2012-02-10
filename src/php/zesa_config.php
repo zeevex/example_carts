6156a44cf73a9b54f8e41019af45f9516bc10979
James Whitfield <jwhitfield@zeevex.com>
2012-02-10 14:50:49 -0500

    added dummy receipt and cancel page. purchase works by click purchase with game-wallet

--- src/php/zesa_config.php
+++ src/php/zesa_config.php
@@ -7,14 +7,14 @@
 require('common.php');
 
 
-$ZESA_HOST="sandbox.zeevex.com";
+$ZESA_HOST="game-wallet.dev";
 $ZESA_PORT=80;
 $ZESA_PATH="/cgi-bin/webscr";
 $ZESA_ACTION = "http://$ZESA_HOST:$ZESA_PORT$ZESA_PATH";
 
-$EXAMPLE_HOST_URL = "http://yourwebsite.com"; // change this to match your server
-$EXAMPLE_CANCEL_RETURN_URL = "$EXAMPLE_HOST_URL/index.php";
-$EXAMPLE_RETURN_URL = "$EXAMPLE_HOST_URL/return.php";
+$EXAMPLE_HOST_URL = "http://phpcart.zeevex.com"; // change this to match your server
+$EXAMPLE_CANCEL_RETURN_URL = "$EXAMPLE_HOST_URL/target_cancel.php";
+$EXAMPLE_RETURN_URL = "$EXAMPLE_HOST_URL/target_receipt.php";
 $EXAMPLE_NOTIFY_URL = "$EXAMPLE_HOST_URL/ipn.php";
 $EXAMPLE_INVOICE_NUMBER = time();
 
@@ -22,7 +22,7 @@ $EXAMPLE_ITEM_NAME = "Ring of Power";
 $EXAMPLE_ITEM_SKU  = rand(2000,3000);
 
 
-$EXAMPLE_BUSINESS = "admin@zeevex.com";
+$EXAMPLE_BUSINESS = "dean@zeevex.com";
 $IPN_LOG="ipn.log";
 $RANDOM_ORDER = rand(1, 1000);
 
